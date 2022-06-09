4ce7da888c9f60a1d9fd37daa188e90ea8ed7cc2
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2022-06-09 10:45:05 -0400

    Add Navigation & Routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+// parent level component;  use Fragment if you don't want a wrapping div -- satisfies React's criteria of just one top-level components
+const Navigation = () => {
+	return (
+		<>
+			<div className="navigation">
+				<Link className="logo-container" to="/">
+					<CrwnLogo className="logo" />
+				</Link>
+				<div className="nav-links-container">
+					<Link className="nav-link" to="/shop">
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Navigation;
