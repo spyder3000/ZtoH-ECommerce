b4248acbcffd706211c2edde33fac395a8f183ae
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2022-06-11 08:15:26 -0400

    Added sign-incomponent as well as Firebase utils and libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,77 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from "firebase/auth"; // getAuth used to create an auth instance
+
+import {
+	getFirestore,
+	doc, // allows us to retrieve documents from firestore;  doc will get you the document instance
+	getDoc, // getting the document data
+	setDoc, // set the document data
+} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBT9YGYmECBp9wkHsKLk8z_IvZMvpg6BSk",
+	authDomain: "crwn-clothing-db-987a3.firebaseapp.com",
+	projectId: "crwn-clothing-db-987a3",
+	storageBucket: "crwn-clothing-db-987a3.appspot.com",
+	messagingSenderId: "837604074344",
+	appId: "1:837604074344:web:1bf636b0fbc94dfde12cdd",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider(); // gives back provider instance;  new because we may might need multiple providers
+
+provider.setCustomParameters({
+	prompt: "select_account", // when someone interacts w/ our provider, we want to force them to select an account (what Google wants)
+});
+
+export const auth = getAuth(); // auth instance;  no 'new' keyword because we only need 1 authentication
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+// async fn that receives some user Authentication object;
+export const createUserDocumentFromAuth = async (userAuth) => {
+	// 2nd param is collection;  3rd is unique identifier
+	const userDocRef = doc(db, "users", userAuth.uid);
+	console.log("createUserDocumentFromAuth");
+	console.log(userDocRef);
+
+	// getDoc allows us methods to check if document exists, & then to create if not;  even if document does
+	//    not exist, the userDocRef tells us where the document will go
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists()); // allows us methods to check document
+
+	// if user data does not exist, create user
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("error creating the user", error.message);
+		}
+	}
+
+	// returns existing OR just created userDocRef;
+	return userDocRef;
+
+	// if user data exists, return user data reference
+};
