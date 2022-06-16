ad3ac2d3301092a800bb3ccd2f0ce977616949c3
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2022-06-16 11:29:16 -0400

    Checkout page and Checkout Items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
 
 import CartItem from "../cart-item/cart-item.component";
@@ -8,13 +10,19 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const goToCheckoutHandler = () => {
+		navigate("/checkout");
+	};
+
 	return (
 		<div className="cart-dropdown-container">
 			<div className="cart-items">
 				{cartItems.map((item) => (
 					<CartItem key={item.id} cartItem={item} />
 				))}
-				<Button>GO TO CHECKOUT</Button>
+				<Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
 			</div>
 		</div>
 	);
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -18,18 +18,49 @@ const addCartItem = (cartItems, productToAdd) => {
 	}
 };
 
+// return new array with modified cartItems
+const removeCartItem = (cartItems, cartItemToRemove) => {
+	// find if cartItems contains cartItemToRemove
+	let existingCartItem = cartItems.find(
+		(item) => item.id === cartItemToRemove.id
+	);
+
+	// Decrement or delete (if modify from 1 to 0) for matched item
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((dat) => dat.id !== existingCartItem.id);
+	} else {
+		return cartItems.map((cartItem) => {
+			return cartItem.id === existingCartItem.id
+				? { ...cartItem, quantity: cartItem.quantity - 1 }
+				: cartItem;
+		});
+	}
+};
+
+// return new array with modified cartItems
+const clearCartItem = (cartItems, cartItemToRemove) => {
+	// find if cartItems contains cartItemToRemove
+	let existingCartItem = cartItems.find(
+		(item) => item.id === cartItemToRemove.id
+	);
+	return cartItems.filter((dat) => dat.id !== existingCartItem.id);
+};
+
 export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
 	cartItems: [],
 	cartCount: 0,
+	cartTotal: 0,
 	addItemToCart: () => {},
+	removeItemFromCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
 	const [cartCount, setCartCount] = useState(0);
+	const [cartTotal, setCartTotal] = useState(0);
 
 	// every time cartItems changes, run this function to re-calculate cartCount
 	useEffect(() => {
@@ -40,9 +71,29 @@ export const CartProvider = ({ children }) => {
 		setCartCount(newCartCount);
 	}, [cartItems]);
 
+	// every time cartItems changes, run this function to re-calculate cartTotal
+	//  ** NOTE:  BEST practice – use just one useEffect() for each variable being governed (& not add to existing useEffect for cartCount
+	useEffect(() => {
+		const newCartTotal = cartItems.reduce(
+			(total, cartItem) => total + cartItem.quantity * cartItem.price,
+			0
+		);
+		setCartTotal(newCartTotal);
+	}, [cartItems]);
+
 	// addCartItem() -- will return updated array of objects
 	const addItemToCart = (productToAdd) => {
 		setCartItems(addCartItem(cartItems, productToAdd));
+	};
+
+	// removeItemFromCart() -- will return updated array of objects
+	const removeItemFromCart = (cartItemToRemove) => {
+		setCartItems(removeCartItem(cartItems, cartItemToRemove));
+	};
+
+	// clearItemFromCart() -- will return updated array of objects
+	const clearItemFromCart = (cartItemToDelete) => {
+		setCartItems(clearCartItem(cartItems, cartItemToDelete));
 	};
 
 	// value is what we want to expose in the Context
@@ -50,8 +101,11 @@ export const CartProvider = ({ children }) => {
 		isCartOpen,
 		setIsCartOpen,
 		addItemToCart,
+		removeItemFromCart,
+		clearItemFromCart,
 		cartItems,
 		cartCount,
+		cartTotal,
 	};
 
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
